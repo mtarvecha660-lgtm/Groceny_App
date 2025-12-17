909f6912d34fd242444e3f0b56b2b6f863cf777f
moksh <mtarvecha660@gmail.com>
2025-12-17 14:07:39 +0530

    add button

--- Groceny_App/ContentView.swift
+++ Groceny_App/ContentView.swift
@@ -13,15 +13,63 @@ struct ContentView: View {
     @Environment(\.modelContext)
     private var modelContext
     
+    @State private var title: String = ""
+    @State private var isAlertShowing: Bool = false
     
     @Query private var lists: [Listt]
     
     var body: some View {
-        VStack {
+        NavigationStack {
+            List{
+                ForEach(lists)
+                {
+                    list in
+                    Text(list.title)
+                        .font(.title2)
+                        .fontWeight(.light)
+                        .padding(.vertical,2)
+                    
+                    
+                }
+            }
+            .navigationTitle("Grocery List")
+            .toolbar{
+                            ToolbarItem(placement:.topBarTrailing){
+                                Button{
+                                    isAlertShowing.toggle()
+                                } label: {
+                                    Image(systemName: "carrot")
+                                        .imageScale(.large)
+                                }
+                            }
+                        }
             
+            .overlay{
+                           if lists.isEmpty{
+                               ContentUnavailableView("Empty Cart",
+                                                      
+                                                      systemImage:"cart.circle",description: Text("Add some shopping items"))
+                           }
+                       }
         }
+    }
+}
+
+#Preview("Second List") {
+    do{
+        let container = try!
+        ModelContainer(for: Listt.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        let ctx = container.mainContext
+        ctx.insert(Listt(title: "Swift Coding Club", isCompleted: true))
+        ctx.insert(Listt(title: "Laundary", isCompleted: true))
+        ctx.insert(Listt(title: "Homework", isCompleted: true))
+        
+        return ContentView()
+            .modelContainer(container)
     }
 }
 #Preview {
     ContentView()
+        .modelContainer(for: Listt.self , inMemory: true)
 }
