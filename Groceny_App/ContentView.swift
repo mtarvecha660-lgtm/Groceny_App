4bdf65b8ea92b0928613f11e4bd5f1a4afcb1865
aadi <shahaadi285@gmail.com>
2025-12-17 10:58:02 +0530

    Initial Commit

---
+++ Groceny_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Groceny_App
+//
+//  Created by students on 17/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Groceny_App/Groceny_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Groceny_AppApp.swift
+//  Groceny_App
+//
+//  Created by students on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Groceny_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
